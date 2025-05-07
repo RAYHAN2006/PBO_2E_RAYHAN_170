b4e96e4423cfbe840a841174c78b28410414d952
RAYHAN <maulanarayhan190@gmail.com>
2025-05-07 22:36:54 +0700

    Add files via upload

---
+++ MODUL_3_TUGAS/Tugas3/src/admin2.java
@@ -0,0 +1,17 @@
+public class admin2 {
+    private String username;
+
+    public admin2(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
+
